fcdcbcabb8641380c883e723bcb6282d8994d2e5
remixan07 <remixan07@gmail.com>
2026-02-06 08:19:59 -0600

    Actividad 3 - Versión 5 - 06/02/26 - 08:19

--- POO_Actividad_3.java
+++ POO_Actividad_3.java
@@ -3,11 +3,21 @@ import java.util.Scanner;
 class Producto {
 
     private String descripcion;
+    private String codigo;
+    private String tipo;
     private double costo;
     private double impuesto;
 
     public void setDescripcion(String x) {
         descripcion = x;
+    }
+
+    public void setCodigo(String x) {
+        codigo = x;
+    }
+
+    public void setTipo(String x) {
+        tipo = x;
     }
 
     public void setCosto(double x) {
@@ -31,41 +41,47 @@ class Producto {
 
 public class POO_Actividad_3 {
 
+    static String compararProductos(Producto p, Producto q) {
+
+        if (p.calcularPrecio(.20) > q.calcularPrecio(.20)) {
+            return p.getDescripcion();
+        } else {
+            return q.getDescripcion();
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        Producto a = new Producto();
-        Producto b = new Producto();
+        Producto p1 = new Producto();
+        Producto p2 = new Producto();
 
-        a.setDescripcion(sc.nextLine());
-
-        try {
-            a.setCosto(Double.parseDouble(sc.nextLine()));
-        } catch (Exception e) {
-            a.setCosto(0);
-        }
-
-        a.setImpuesto(.16);
-
-        b.setDescripcion(sc.nextLine());
+        p1.setDescripcion(sc.nextLine());
+        p1.setCodigo(sc.nextLine());
+        p1.setTipo(sc.nextLine());
 
         try {
-            b.setCosto(Double.parseDouble(sc.nextLine()));
+            p1.setCosto(Double.parseDouble(sc.nextLine()));
         } catch (Exception e) {
-            b.setCosto(0);
+            p1.setCosto(0);
         }
 
-        b.setImpuesto(.16);
+        p1.setImpuesto(.16);
 
-        double pa = a.calcularPrecio(.20);
-        double pb = b.calcularPrecio(.20);
+        p2.setDescripcion(sc.nextLine());
+        p2.setCodigo(sc.nextLine());
+        p2.setTipo(sc.nextLine());
 
-        if (pa > pb) {
-            System.out.println(a.getDescripcion());
-        } else {
-            System.out.println(b.getDescripcion());
+        try {
+            p2.setCosto(Double.parseDouble(sc.nextLine()));
+        } catch (Exception e) {
+            p2.setCosto(0);
         }
+
+        p2.setImpuesto(.16);
+
+        System.out.println(compararProductos(p1, p2));
 
         sc.close();
     }
